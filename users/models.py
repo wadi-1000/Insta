b6ced997917ed6cfd685da16a775206df4d38902
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-12 19:56:44 +0300

    Added Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    image = CloudinaryField(default='default.jpeg')
+    bio = models.TextField( default="Please Update Bio")
 
-# Create your models here.
+    def __str__(self):
+        return '{} {}'.format(self.image.url, self.bio)
+
+
+    def save_profile(self):
+        self.save()
