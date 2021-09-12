a53c3fe38c6ae8796b8a2bea302bdb61592c882a
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-12 20:05:49 +0300

    Added view function for registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import UserRegisterForm
+from .models import Profile
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('login')
+
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/registration.html', {"form":form})
