2e3b53cef11d6bbe6914242efea2312fb9e69ffe
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-12 20:03:57 +0300

    Imported User Model to be used in usercreationform

--- users/forms.py
+++ users/forms.py
@@ -6,3 +6,7 @@ from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
