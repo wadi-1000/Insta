625d44845c113030bddc8903d8a42262fffdf5d5
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-12 21:08:50 +0300

    Added path for profile

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('accounts/register/', user_views.register, name ='register'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    
+    path('profile/', user_views.profile, name = 'profile'),
+
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
