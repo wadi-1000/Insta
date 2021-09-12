264031013f24a0f641a8a075116e17c25867c731
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-12 20:01:02 +0300

    Added test to test Profile Model

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Profile
 
-# Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.joy=Profile(bio = 'Hello World')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.joy.Profile))
+    
+    def test_save_method(self):
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)
